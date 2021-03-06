7649f4b9393e7bfaf42af80843baee44fc6427af
James Radford <radford.james@gmail.com>
2021-03-06 10:55:50 +0700

    Correct To-Do unit test

--- lists/tests.py
+++ lists/tests.py
@@ -14,5 +14,5 @@ class HomePageTest(TestCase):
         response = home_page(request)
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To_Do List</title>', html)
+        self.assertIn('<title>To-Do List</title>', html)
         self.assertTrue(html.endswith('</html>'))
